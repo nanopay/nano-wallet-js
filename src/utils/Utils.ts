80ea5fc123ce63a768a637a7c789b9c252beb50e
anarkrypto <anarkrypto@gmail.com>
2023-07-03 05:02:41 -0300

    ✨ Add Utils and Constants

---
+++ src/utils/Utils.ts
@@ -0,0 +1,14 @@
+export interface FetchWithTimeoutOptions extends Omit<RequestInit, 'signal'> {
+    timeout: number;
+}
+
+export const fetchWithTimeout = async (url: string, { timeout, ...options }: FetchWithTimeoutOptions): Promise<Response> => {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), timeout);
+    const response = await fetch(url, {
+        ...options,
+        signal: controller.signal
+    });
+    clearTimeout(timeoutId);
+    return response;;
+}
