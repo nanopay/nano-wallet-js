ad00789e82a623a6ac5b82964aa85461e239d51e
anarkrypto <anarkrypto@gmail.com>
2023-07-22 17:16:46 -0300

    🔧 Add jest environment fail fast

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	roots: ['<rootDir>/src'],
 	preset: 'ts-jest',
-	testEnvironment: 'node',
+	testEnvironment: './jest-environment-fail-fast.ts',
 	testPathIgnorePatterns: ['node_modules'],
 	verbose: true,
 	moduleNameMapper: {
@@ -18,4 +18,6 @@ module.exports = {
 			statements: 100,
 		},
 	},
+	testRunner: 'jest-circus/runner',
+
 };
