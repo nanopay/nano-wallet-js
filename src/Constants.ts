24a3e58cbf0eae2a5d286336bc9e87a7a1f81325
anarkrypto <anarkrypto@gmail.com>
2023-07-22 17:17:14 -0300

    🔧 Increase DEFAULT_TIMEOUT to 1min

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,4 +1,4 @@
 export const SEND_DIFFICULTY = 'fffffff800000000';
 export const RECEIVE_DIFFICULTY = 'fffffe0000000000';
 export const MIN_AMOUNT = 0.00001;
-export const DEFAULT_TIMEOUT = 30 * 1000; // 30 seconds
+export const DEFAULT_TIMEOUT = 60 * 1000; // 60 seconds
